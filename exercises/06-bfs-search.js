a3ad7e32262e7a9ea3b7ffc48bcc479bffc61366
xuman <xuman@list.ru>
2018-02-25 19:22:00 +0300

    Устранены предупреждения линтера

--- exercises/06-bfs-search.js
+++ exercises/06-bfs-search.js
@@ -8,37 +8,36 @@ const testTree = {
   data: {
     type: 'Random',
     output: {
-      '1': { responce: { some: 'Some error found! Bingo!' } },
-      '2': { responce: { second: 'Second error found! Bingo!' } },
-      '3': { responce: { another: 'Another error found! Bingo!' } },
-      '4': { responce: { text: 'The shiny text found! Bingo!' } },
-      '5': { responce: { last: 'Last error found! Bingo!' } },
-      '6': { someStub: { here: { lies: { some: { treasure: 1000000 } } } } }
+      one: { responce: { some: 'Some error found! Bingo!' } },
+      two: { responce: { second: 'Second error found! Bingo!' } },
+      three: { responce: { another: 'Another error found! Bingo!' } },
+      four: { responce: { text: 'The shiny text found! Bingo!' } },
+      five: { responce: { last: 'Last error found! Bingo!' } },
+      six: { someStub: { here: { lies: { some: { treasure: 1000000 } } } } }
     }
   }
-}
+};
 
 const bfsSearch = (tree, nodeName) => {
-
   const search = (subtree) => {
-    const keys = Object.keys(subtree)
-    if (keys.indexOf(nodeName) !== -1) return subtree[nodeName]
+    const keys = Object.keys(subtree);
+    if (keys.indexOf(nodeName) !== -1) return subtree[nodeName];
 
-    for (let i = 0; i < keys.length; i++) {
+    for (let i = 0; i < keys.length; i += 1) {
       const key = keys[i];
-      if (typeof subtree[key] !== 'object') continue
-      const check = search(subtree[key])
-      if (!!check) return check
+      if (typeof subtree[key] !== 'object') continue; // eslint-disable-line no-continue
+      const check = search(subtree[key]);
+      if (check) return check;
     }
-    return null
-  }
+    return null;
+  };
 
-  return search(tree)
-}
+  return search(tree);
+};
 
-/*******************************************************************************
+/*
 ==================================>TESTS<=======================================
-*******************************************************************************/
+*/
 
 // console.log(bfsSearch(testTree, 'some'));
 // console.log(bfsSearch(testTree, 'second'));
@@ -50,25 +49,25 @@ const bfsSearch = (tree, nodeName) => {
 assert.equal(
   bfsSearch(testTree, 'some'),
   'Some error found! Bingo!'
-)
+);
 assert.equal(
   bfsSearch(testTree, 'second'),
   'Second error found! Bingo!'
-)
+);
 assert.equal(
   bfsSearch(testTree, 'another'),
   'Another error found! Bingo!'
-)
+);
 assert.equal(
   bfsSearch(testTree, 'text'),
   'The shiny text found! Bingo!'
-)
+);
 assert.equal(
   bfsSearch(testTree, 'last'),
   'Last error found! Bingo!'
-)
+);
 assert.equal(
   bfsSearch(testTree, 'treasure'),
   1000000
-)
+);
 
