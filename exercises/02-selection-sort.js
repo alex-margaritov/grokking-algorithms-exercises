d3df44208fddc2158cf8751ef4f3e09c3c01103a
xuman <xuman@list.ru>
2018-02-25 15:28:22 +0300

    Устранены предупреждения линтера

--- exercises/02-selection-sort.js
+++ exercises/02-selection-sort.js
@@ -5,41 +5,41 @@
 const assert = require('assert');
 
 const testArray = [
-    55, 92, 25, 79, 4, 14, 81, 49, 87, 70, 4, 84, 39, 41, 79, 86, 0, 41, 22, 26,
-    45, 71, 81, 34, 44, 72, 67, 90, 62, 39, 76, 19, 55, 15, 6, 57, 48, 94, 94,
-    43, 23, 30, 94, 46
-]
+  55, 92, 25, 79, 4, 14, 81, 49, 87, 70, 4, 84, 39, 41, 79, 86, 0, 41, 22, 26,
+  45, 71, 81, 34, 44, 72, 67, 90, 62, 39, 76, 19, 55, 15, 6, 57, 48, 94, 94,
+  43, 23, 30, 94, 46,
+];
 
 // Сортировка чисел по убыванию
 const descentSort = (list) => {
-    const result = []
-    const innerList = list.slice()
+  const result = [];
+  const innerList = list.slice();
 
-    const findMaxIndex = (list) => {
-        let max = Number.MIN_VALUE
-        let index
-        for (let i = 0; i < list.length; i++) {
-            if (list[i] > max) { max = list[i]; index = i }
-        }
-        return index
+  const findMaxIndex = (arr) => {
+    let max = Number.MIN_VALUE;
+    let index;
+    for (let i = 0; i < arr.length; i += 1) {
+      if (arr[i] > max) { max = arr[i]; index = i; }
     }
+    return index;
+  };
 
-    while (innerList.length > 0) {
-        const currentMaxIndex = findMaxIndex(innerList)
-        const [currentMax] = innerList.splice( currentMaxIndex, 1 )
-        result.push(currentMax)
-    }
+  while (innerList.length > 0) {
+    const currentMaxIndex = findMaxIndex(innerList);
+    const [currentMax] = innerList.splice(currentMaxIndex, 1);
+    result.push(currentMax);
+  }
 
-    return result
-}
+  return result;
+};
 
-/*******************************************************************************
+/*
 ==================================>TESTS<=======================================
-*******************************************************************************/
+*/
 
 // Testing descentSort
 // console.log('Result:\n[', descentSort(testArray).join(', '), ']');
 assert.deepEqual(
-    descentSort(testArray),
-    testArray.sort( (a, b) => b - a )
-)
+  descentSort(testArray),
+  testArray.sort((a, b) => b - a),
+);
