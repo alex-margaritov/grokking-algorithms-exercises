199263cb2fb2322021caa52d5d8c1a3890bc54ff
xuman <xuman@list.ru>
2018-02-25 16:14:28 +0300

    comma-dangle rule fix

--- exercises/01-binary-search.js
+++ exercises/01-binary-search.js
@@ -8,7 +8,7 @@ const assert = require('assert');
 const testArray = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 17, 18, 19, 20, 21, 22,
   23, 24, 25, 26, 27, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
-  44, 45,
+  44, 45
 ];
 
 
@@ -77,7 +77,7 @@ for (let i = 0; i < 50; i += 1) {
   // console.log('Result:', stupidBinarySearch(i, testArray))
   assert.equal(
     stupidBinarySearch(i, testArray),
-    testArray.indexOf(i) > -1,
+    testArray.indexOf(i) > -1
   );
 }
 
@@ -86,7 +86,7 @@ for (let i = 0; i < 50; i += 1) {
   // console.log('Result:', recursiveBinarySearch(i, testArray))
   assert.equal(
     recursiveBinarySearch(i, testArray),
-    testArray.indexOf(i) > -1,
+    testArray.indexOf(i) > -1
   );
 }
 
@@ -95,6 +95,6 @@ for (let i = 0; i < 50; i += 1) {
   // console.log('Result:', customIndexOf(i, testArray))
   assert.equal(
     customIndexOf(i, testArray) > -1,
-    testArray.indexOf(i) > -1,
+    testArray.indexOf(i) > -1
   );
 }
--- exercises/02-selection-sort.js
+++ exercises/02-selection-sort.js
@@ -7,7 +7,7 @@ const assert = require('assert');
 const testArray = [
   55, 92, 25, 79, 4, 14, 81, 49, 87, 70, 4, 84, 39, 41, 79, 86, 0, 41, 22, 26,
   45, 71, 81, 34, 44, 72, 67, 90, 62, 39, 76, 19, 55, 15, 6, 57, 48, 94, 94,
-  43, 23, 30, 94, 46,
+  43, 23, 30, 94, 46
 ];
 
 // Сортировка чисел по убыванию
@@ -41,5 +41,5 @@ const descentSort = (list) => {
 // console.log('Result:\n[', descentSort(testArray).join(', '), ']');
 assert.deepEqual(
   descentSort(testArray),
-  testArray.sort((a, b) => b - a),
+  testArray.sort((a, b) => b - a)
 );
